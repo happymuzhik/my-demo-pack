cc8c5e069a7db8e66615656d5ebb973e9d4d9889
happymuzhik <ber_kut_91@mail.ru>
2017-09-18 18:44:55 +0300

    same

--- src/create-src-dir.js
+++ src/create-src-dir.js
@@ -8,17 +8,14 @@ module.exports = function(work_dir) {
 
     const SRC_DIR = mkDirCustom(work_dir, 'src');
     createFile(work_dir + SRC_DIR, 'index.js');
+    copyFile(__ASSETS + HTML_FILE, work_dir + SRC_DIR + HTML_FILE);
 
     const APP_DIR = mkDirCustom(work_dir, SRC_DIR + 'app');    
-
-    const HTML_DIR = mkDirCustom(work_dir, SRC_DIR + 'html');
-    copyFile(__ASSETS + HTML_FILE, work_dir + HTML_DIR + HTML_FILE);
-    
     const COMMON_DIR = mkDirCustom(work_dir, APP_DIR + 'common');
-    const UTILS_DIR = mkDirCustom(work_dir, APP_DIR + 'utils');
-    const COMPS_DIR = mkDirCustom(work_dir, APP_DIR + 'components');
-    const ASSETS_DIR = mkDirCustom(work_dir, APP_DIR + 'assets');
-    const STYLES_DIR = mkDirCustom(work_dir, APP_DIR + 'styles');
+    const UTILS_DIR = mkDirCustom(work_dir, COMMON_DIR + 'utils');
+    const COMPS_DIR = mkDirCustom(work_dir, COMMON_DIR + 'components');
+    const ASSETS_DIR = mkDirCustom(work_dir, COMMON_DIR + 'assets');
+    const STYLES_DIR = mkDirCustom(work_dir, COMMON_DIR + 'styles');
     createFile(work_dir + STYLES_DIR, 'common.styl');
 
 }
